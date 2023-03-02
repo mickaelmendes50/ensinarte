a2ba416ff35a0883bc2efa99d0d5c3faafbd0c30
Mesquita <mesquita@aospa.co>
2023-03-01 23:11:31 -0300

    ensinarte: initialize partiton
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- installer.bat
+++ installer.bat
@@ -1,4 +1,11 @@
 @echo off
+set LOCAL_PATH=%cd%
+
 :: Iniciando o processo
-echo Iniciando instalacao, aguarde...
-timeout /t 5 /nobreak > NUL
+echo Iniciando configuracao, aguarde...
+timeout /t 2 /nobreak > NUL
+
+:: Cria o disco Z com label ENSINARTE
+diskpart /s %LOCAL_PATH%/script/partition.txt
+echo final
+timeout /t 5 /nobreak
